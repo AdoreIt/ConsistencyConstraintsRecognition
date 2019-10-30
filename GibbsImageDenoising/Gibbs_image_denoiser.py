94d86bfbe4ed1cfee9b14736c582fbb0f625fc74
AdoreIt <NatalieLytvyniuk@gmail.com>
2019-10-30 19:59:31 +0200

    Start numpy impl

---
+++ GibbsImageDenoising/Gibbs_image_denoiser.py
@@ -0,0 +1,15 @@
+import numpy as np
+
+def get_neighbors(row, col):
+    """ returns array of neighbors
+    [0,0,0,None]
+    """
+
+def generate_image(height, width):
+    random_image = np.random.randint(2, size=(height, width))
+    for row in range(height):
+        for col in range(width):
+
+
+if __name__ == "__main__":
+    print("Hello world")
